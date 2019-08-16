6a7594d08c40f2eb1cdb48e2a47b3fb0498cb644
417-72KI <417.72ki@gmail.com>
2019-08-16 15:55:17 +0900

    Add files

---
+++ Sources/SegueCollections/FlipPresentSegue.swift
@@ -0,0 +1,12 @@
+//
+//  FlipPresentSegue.swift
+//  SegueCollections
+//
+//  Created by 417.72KI on 2018/09/21.
+//
+
+import UIKit
+
+class FlipPresentSegue: UIStoryboardSegue {
+
+}
---
+++ Sources/SegueCollections/Protocols/Animation/FlipAnimatable.swift
@@ -0,0 +1,8 @@
+//
+//  FlipAnimatable.swift
+//  SegueCollections
+//
+//  Created by 417.72KI on 2018/09/21.
+//
+
+import Foundation
